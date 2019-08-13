d32d840b36964f5e2cba973227e9c28b042f35fe
Charles Kanakan <50149696+CKanakan@users.noreply.github.com>
2019-08-13 14:40:22 -0400

    Add files via upload

---
+++ mpg_calculator.py
@@ -0,0 +1,6 @@
+milesDriven=input("Enter Miles driven: ")
+milesDriven= int(milesDriven)
+gallonsUsed=input("Enter Gallon used: ")
+gallonsUsed= int(gallonsUsed)
+mpg=milesDriven/gallonsUsed
+print(mpg)
